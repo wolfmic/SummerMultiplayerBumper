fc7d025a714f4324a162176a14e0d073c3992837
Aymeric Girault <aymeric.girault@epitech.eu>
2013-07-28 14:40:50 +0200

    Menu added

---
+++ Assets/MyAssets/Scripts/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public string CurrentMenu = "Main";
+    
+
+    string ServerName = "server name";
+
+    void Start() {
+    }
+
+    void OnGUI() {
+        if (CurrentMenu == "Main") {
+            MainMenu();
+        } else if (CurrentMenu == "Lobby") {
+            LobbyMenu();
+        } else if (CurrentMenu == "Host") {
+            HostMenu();
+        }
+    }
+
+    void Navigate(string s) {
+        CurrentMenu = s;
+    }
+
+    void MainMenu() {
+        if (GUI.Button(new Rect(10, 10, 200, 50), "Host Game")) {
+            Navigate("Host");
+        } else if (GUI.Button(new Rect(10, 20 + 50, 200, 50), "Join")) {
+            Navigate("Lobby");
+        }
+    }
+
+    void LobbyMenu() {
+        if (GUI.Button(new Rect(10, 10, 200, 50), "Back")) {
+            Navigate("Main");
+        }
+    }
+
+    void HostMenu() {
+        if (GUI.Button(new Rect(10, 10, 200, 50), "Back")) {
+            Navigate("Main");
+        } else if (GUI.Button(new Rect(10, 20 + 50, 200, 50), "Start Server")) {
+            
+        }
+
+        GUI.Label(new Rect(10, 200, 200, 50), "Name:");
+        ServerName = GUI.TextField(new Rect(100, 200, 200, 20), ServerName);
+    }
+}
---
+++ Assets/MyAssets/Scripts/Network/MultiplayerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerManager : MonoBehaviour {
+
+    public int maxplayers = 4;
+
+    string servername;
+
+    public void StartServer() {
+        Network.InitializeServer(maxplayers, 25003, false);
+    }
+
+    void OnServerInitialized() {
+        Debug.Log("Hello");
+    }
+
+}
